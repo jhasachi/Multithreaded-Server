431fc1bad296d7f02a4c5bf4c38c7a6f079418c0
Sachin <jhasachi@outlook.com>
2023-02-20 21:07:49 +0100

    Completed 1st, 2nd and 3rd test.

--- robo.py
+++ robo.py
@@ -3,63 +3,113 @@ import msg
 
 def find_point(client_socket):
 
-    response = move_forward(client_socket, msg.SERVER_MOVE)
-    tokens = response[0:-2].split()
-    curr_x, curr_y = int(tokens[1]), int(tokens[2])
+    new_x, new_y = -1, -1
+    curr_x, curr_y = -1, -1
 
-    if curr_x == 0 and curr_y == 0:
-        reach_origin(client_socket, curr_x, curr_y)
-
-    response = move_forward(client_socket, msg.SERVER_MOVE)
-    tokens = response[0:-2].split()
-    new_x, new_y = int(tokens[1]), int(tokens[2])
-
-    if new_y == 0 and new_x == 0:
-        reach_origin(client_socket, new_x, new_y)
-
-    find_position(client_socket, curr_x, curr_y, new_x, new_y)
-
-def find_position(client_socket, current_x, current_y, new_x, new_y):
-
-    # Handle positions on the horizontal and vertical axis.
-    if  current_y == new_y:
-        if current_x < new_x:
-            # Robot is on the horizontal axis, facing east.
-            if current_x >= 0:
-                rotate_right(client_socket)
-                rotate_right(client_socket)
-                reach_origin(client_socket, new_x, new_y)
-            elif current_x <= 0:
-                reach_origin(client_socket, new_x, new_y)
-        elif current_x > new_x:
-            # Robot is on the horizontal axis, facing west.
-            rotate_right(client_socket)
-            rotate_right(client_socket)
-            reach_origin(client_socket, new_x, new_y)
-    elif current_x == new_x:
-        if current_y < new_y:
-            # Robot is on the vertical axis, facing north.
-            rotate_right(client_socket)
-            rotate_right(client_socket)
-            reach_origin(client_socket, new_x, new_y)
-        elif current_y > new_y:
-            # Robot is on the vertical axis, facing south.
-            rotate_right(client_socket)
-            rotate_right(client_socket)
-            reach_origin(client_socket, new_x, new_y)
-
-    # Position for point on one of the four quadrant
-
-def reach_origin(client_socket, x1, y1):
-
-    while x1 != 0 and y1 != 0:
+    for i in range(2):
         response = move_forward(client_socket, msg.SERVER_MOVE)
-        tokens = response.split()
-        x1, y1 = int(tokens[1]), int(tokens[2])
+        tokens = response[0:-2].split()
+        x, y = int(tokens[1]), int(tokens[2])
 
-    pick_message(client_socket)
+        if i == 0:
+            curr_x, curr_y = x, y
+            if curr_x == 0 and curr_y == 0:
+                reach_origin(client_socket, curr_x, curr_y)
+
+        if i == 1:
+            new_x, new_y = x, y
+            if new_x == 0 and new_y == 0:
+                reach_origin(client_socket, new_x, new_y)
+
+    find_method(new_x, new_y, find_direction(new_x, new_y, curr_x, curr_y), client_socket)
+
+def find_direction(curr_x, curr_y, new_x, new_y):
+
+    direction = -1
+    # Assigning the value according to direction: North "1" , South "3" , West "2" , East "4"
+
+    if curr_x > new_x:
+        direction = 2
+    elif curr_x < new_x:
+        direction = 4
+    elif curr_y > new_y:
+        direction = 3
+    elif curr_y < new_y:
+        direction = 1
+    return direction
+
+def find_method(x, y, direction, client_socket):
+
+    if direction == 1:
+        if y >= 0:
+            for i in range(2):
+                rotate_right(client_socket)
+                value = 3
+                move_to_abscissa(client_socket, x, y, value)
+
+        elif y < 0:
+            value = 1
+            move_to_abscissa(client_socket, x, y, value)
+
+    if direction == 2:
+        if y >= 0:
+            rotate_left(client_socket)
+            value = 3
+            move_to_abscissa(client_socket, x, y, value)
+        elif y < 0:
+            rotate_right(client_socket)
+            value = 1
+            move_to_abscissa(client_socket, x, y, value)
+
+    if direction == 3:
+        if y >= 0:
+            value = 3
+            move_to_abscissa(client_socket, x, y, value)
+        elif y < 0:
+            for i in range(2):
+                rotate_right(client_socket)
+                value = 1
+                move_to_abscissa(client_socket, x, y, value)
+    if direction == 4:
+        if y >= 0:
+            rotate_right(client_socket)
+            value = 3
+            move_to_abscissa(client_socket, x, y, value)
+        elif y < 0:
+            rotate_left(client_socket)
+            value = 1
+            move_to_abscissa(client_socket, x, y, value)
+
+def move_to_abscissa(client_socket, x, y, direction):
+
+    while y != 0:
+        response = move_forward(client_socket, msg.SERVER_MOVE)
+        token = response.split()
+        x, y = int(token[1]), int(token[2])
+
+    if direction == 3:
+            rotate_left(client_socket)
+    if direction == 1:
+            rotate_right(client_socket)
+
+    move_to_ordinates(client_socket, x, y)
+
+def move_to_ordinates(client_socket, x, y):
+
+    while x != 0:
+        response = move_forward(client_socket, msg.SERVER_MOVE)
+        token = response.split()
+        x, y = int(token[1]), int(token[2])
+
+    reach_origin(client_socket, x, y)
+
+def reach_origin(client_socket, x, y):
+
+    if x == 0 and y == 0:
+        pick_message(client_socket)
 
 def pick_message(client_socket):
+
     send_command(client_socket, msg.SERVER_PICK_UP)
     read_response(client_socket)
     send_command(client_socket,msg.SERVER_LOGOUT)
@@ -67,26 +117,15 @@ def pick_message(client_socket):
 
 def build_connection(client_socket):
 
-    server_data = read_response(client_socket)[:-2]
+    hash_value = find_hash(client_socket)
 
-    n = list()
-    for x in server_data:
-        n.append(ord(x))
-
-    value = 0
-    for num in n:
-        value = value + num
-
-    hash_value = (value * 1000) % 65536
-
-    send_command(client_socket,msg.SERVER_KEY_REQUEST)
-    key_data = int(read_response(client_socket)[:-2])
+    key_data = read_key(client_socket, msg.SERVER_KEY_REQUEST)
 
     server_values = {0: 23019, 1: 32037, 2: 18789, 3: 16443, 4: 18189}
     server_hash = (server_values[key_data] + hash_value) % 65536
 
-    send_command(client_socket,f"{server_hash}\a\b")
-    server_confirmation = int(read_response(client_socket)[:-2])
+    server_confirmation = read_confirmation(client_socket, f"{server_hash}\a\b")
+    server_confirmation = int(server_confirmation[:-2])
 
     clients_values = {0: 32037, 1: 29295, 2: 13603, 3: 29533, 4: 21952}
     client_hash = (clients_values[key_data] + hash_value) % 65536
@@ -98,20 +137,95 @@ def build_connection(client_socket):
         send_command(client_socket, msg.SERVER_LOGIN_FAILED)
         client_socket.close()
 
+def find_hash(client_socket):
+    server_data = read_username(client_socket)[:-2]
+
+    n = list()
+    for x in server_data:
+        n.append(ord(x))
+
+    value = 0
+    for num in n:
+        value = value + num
+
+    hash_value = (value * 1000) % 65536
+
+    return hash_value
+
+def rotate_right(client_socket):
+    send_command(client_socket, msg.SERVER_TURN_RIGHT)
+
+    value = read_response(client_socket)
+    if len(value) > 12:
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    return value
+
+def rotate_left(client_socket):
+    send_command(client_socket, msg.SERVER_TURN_LEFT)
+
+    value = read_response(client_socket)
+    if len(value) > 12:
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    return value
+
+def move_forward(client_socket, command):
+    send_command(client_socket, command)
+
+    value = read_response(client_socket)
+    if len(value) > 12:
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    return value
+
+def read_key(client_socket, command):
+    send_command(client_socket, command)
+
+    value = read_response(client_socket)
+    if len(value) > 5:
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    value = int(value[:-2])
+    if value > 4 or value < 0:
+        send_command(client_socket, msg.SERVER_KEY_OUT_OF_RANGE_ERROR)
+        client_socket.close()
+
+    return value
+
+def read_username(client_socket):
+
+    value = read_response(client_socket)
+
+    if not is_valid_username(value):
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    return value
+
+def is_valid_username(message):
+
+    if not message or len(message) > 20 or not message.endswith("\a\b"):
+        return False
+
+    return True
+
+def read_confirmation(client_socket, command):
+    send_command(client_socket, command)
+
+    value = read_response(client_socket)
+    if len(value) > 7:
+        send_command(client_socket, msg.SERVER_SYNTAX_ERROR)
+        client_socket.close()
+
+    return value
+
 def send_command(client_socket, command):
     client_socket.sendall(command.encode())
 
 def read_response(client_socket):
     return client_socket.recv(1024).decode()
-
-def rotate_right(client_socket):
-    send_command(client_socket, msg.SERVER_TURN_RIGHT)
-    read_response(client_socket)
-
-def rotate_left(client_socket):
-    send_command(client_socket, msg.SERVER_TURN_LEFT)
-    read_response(client_socket)
-
-def move_forward(client_socket, command):
-    send_command(client_socket, command)
-    return read_response(client_socket)
